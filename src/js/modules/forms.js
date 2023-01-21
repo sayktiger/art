9304e8ff30a152a2f9bc7b2cce94011d7cb03db1
Konstantin <sayk.tiger09@gmail.com>
2023-01-21 20:04:07 +0300

    add dinamic cards json files

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,11 +1,10 @@
-// import checkNumInputs from './checkNumInputs';
+import { postData } from "../services/requests";
 
 const forms = () => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'),
           upload = document.querySelectorAll(`[name = "upload"]`);
 
-    // checkNumInputs('input[name="user_phone"]');
     
     const message = {
         loading: 'Загрузка...',
@@ -20,15 +19,6 @@ const forms = () => {
         designer: 'assets/server.php',
         question: 'assets/question.php'
     }
-
-    const postData = async (url, data) => {
-        let res = await fetch(url, {
-            method: "POST",
-            body: data
-        });
-
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
